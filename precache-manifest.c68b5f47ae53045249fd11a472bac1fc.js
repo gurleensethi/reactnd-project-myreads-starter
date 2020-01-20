3b34fc9229418d3ae8852c9640ac026ee474962d
Gurleen <sarusethi@rocketmail.com>
2020-01-20 07:28:12 +0000

    Update

---
+++ precache-manifest.c68b5f47ae53045249fd11a472bac1fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e72a6ea8511e9cfa2fd9ca356d596ec",
+    "url": "/reactnd-project-myreads-starter/index.html"
+  },
+  {
+    "revision": "04860b0a11498981291d",
+    "url": "/reactnd-project-myreads-starter/static/css/main.4f85bc34.chunk.css"
+  },
+  {
+    "revision": "6310b4fa1870eabf9998",
+    "url": "/reactnd-project-myreads-starter/static/js/2.c7c07d60.chunk.js"
+  },
+  {
+    "revision": "89e490ec1a74a1e3265c5ed6873a182d",
+    "url": "/reactnd-project-myreads-starter/static/js/2.c7c07d60.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04860b0a11498981291d",
+    "url": "/reactnd-project-myreads-starter/static/js/main.e16ca8e9.chunk.js"
+  },
+  {
+    "revision": "80348379d5589de28ae9",
+    "url": "/reactnd-project-myreads-starter/static/js/runtime-main.19617d95.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/reactnd-project-myreads-starter/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/reactnd-project-myreads-starter/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
